6d5cdec6f184d50da77e94aadc18da0c0068dd6d
theobvious <olga.lempert@gmail.com>
2018-03-06 10:09:40 +0200

    Add files via upload

--- theschool_ol.sql
+++ theschool_ol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2018 at 07:03 PM
+-- Generation Time: Mar 06, 2018 at 09:06 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 5.6.32
 
@@ -32,7 +32,7 @@ CREATE TABLE `courses` (
   `id` int(11) NOT NULL,
   `name` text COLLATE utf8_bin,
   `description` text COLLATE utf8_bin,
-  `image` blob
+  `image` varchar(255) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -40,16 +40,16 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `name`, `description`, `image`) VALUES
-(1, 'Math 101', 'Counting', NULL),
-(2, 'Literature 101', 'Reading', NULL),
-(3, 'Physics 101', 'How Earth works', NULL),
-(4, 'Chemistry 101', 'Mixing things', NULL),
-(5, 'Psychology 101', 'How people work', NULL),
-(6, 'Home Economics 101', 'Making things', NULL),
-(7, 'Gym', 'Stay healthy', NULL),
-(8, 'Biology 101', 'How everything lives', NULL),
-(9, 'Music 101', 'Mixing sounds', NULL),
-(10, 'Finance 101', 'Making money', NULL);
+(1, 'Math 101', 'Counting', 'upload/course.png'),
+(2, 'Literature 101', 'Reading', 'upload/course.png'),
+(3, 'Physics 101', 'How Earth works', 'upload/course.png'),
+(4, 'Chemistry 101', 'Mixing things', 'upload/course.png'),
+(5, 'Psychology 101', 'How people work', 'upload/course.png'),
+(6, 'Home Economics 101', 'Making things', 'upload/course.png'),
+(7, 'Gym', 'Stay healthy', 'upload/course.png'),
+(8, 'Biology 101', 'How everything lives', 'upload/course.png'),
+(9, 'Music 101', 'Mixing sounds', 'upload/course.png'),
+(10, 'Finance 101', 'Making money', 'upload/course.png');
 
 -- --------------------------------------------------------
 
@@ -72,23 +72,22 @@ INSERT INTO `courses_students` (`course_id`, `student_id`) VALUES
 (1, 9),
 (2, 3),
 (2, 6),
+(3, 1),
 (3, 8),
 (3, 9),
 (4, 1),
-(4, 7),
+(4, 2),
 (5, 3),
 (5, 6),
-(5, 7),
-(5, 10),
-(6, 1),
 (6, 6),
 (7, 4),
 (7, 5),
 (7, 7),
 (8, 2),
+(8, 7),
 (9, 2),
-(9, 10),
-(10, 5);
+(9, 11),
+(10, 7);
 
 -- --------------------------------------------------------
 
@@ -101,7 +100,7 @@ CREATE TABLE `students` (
   `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `phone` varchar(100) COLLATE utf8_bin DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `image` blob
+  `image` varchar(255) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -109,16 +108,16 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `name`, `phone`, `email`, `image`) VALUES
-(1, 'Kevyn', '054-7151493', 'kevyn@kevyn.net', NULL),
-(2, 'Malachi', '054-5058743', 'malachi@mail.com', NULL),
-(3, 'Arthur', '054-6948242', 'arthur@king.co.uk', NULL),
-(4, 'Lucy', '054-6980941', 'lucy@narnia.com', NULL),
-(5, 'Lesley', '054-4579028', 'lesley@durrell.com', NULL),
-(6, 'Quintessa', '054-4208103', 'quintessa@d.co', NULL),
-(7, 'Samuel', '054-9473303', 'sam@uel.com', NULL),
-(8, 'Bernard', '054-5364296', 'bernie@ftw.com', NULL),
-(9, 'Bethany', '054-0021393', 'bethanie@pretty.io', NULL),
-(10, 'Lani', '054-8497660', 'lani@lani.la', NULL);
+(1, 'Kevyn', '054-7151495', 'kevyn@kevynn.net', 'upload/student.png'),
+(2, 'Malachi', '054-5058743', 'malachi@mail.com', 'upload/student.png'),
+(3, 'Arthur', '054-6948242', 'arthur@king.co.uk', 'upload/student.png'),
+(4, 'Lucy', '054-6980941', 'lucy@narnia.com', 'upload/student.png'),
+(5, 'Lesley', '054-4579028', 'lesley@durrell.com', 'upload/student.png'),
+(6, 'Adelina', '054-4208103', 'adel@ina.co', 'upload/student.png'),
+(7, 'Samuel', '054-9473303', 'sam@uel.com', 'upload/student.png'),
+(8, 'Bernard', '054-5364296', 'bernie@ftw.com', 'upload/student.png'),
+(9, 'Bethany', '054-0021393', 'bethanie@pretty.io', 'upload/student.png'),
+(11, 'Lani', '054-8887777', 'lani@lani.com', 'upload/student.png');
 
 -- --------------------------------------------------------
 
@@ -132,17 +131,19 @@ CREATE TABLE `users` (
   `phone` varchar(20) COLLATE utf8_bin NOT NULL,
   `email` varchar(30) COLLATE utf8_bin NOT NULL,
   `role` text COLLATE utf8_bin,
-  `pass` text COLLATE utf8_bin
+  `pass` text COLLATE utf8_bin,
+  `image` varchar(255) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `phone`, `email`, `role`, `pass`) VALUES
-(1, 'John Snow', '054-5555555', 'john@snow.com', 'sales', '123'),
-(2, 'Anna Banana', '070-7770000', 'anna@theschool.com', 'owner', '456'),
-(3, 'Elsa Frozen', '067-9876543', 'elsa@brrr.com', 'admin', '123');
+INSERT INTO `users` (`id`, `name`, `phone`, `email`, `role`, `pass`, `image`) VALUES
+(1, 'John Snow', '054-5555555', 'john@snow.com', 'sales', '123', 'upload/admin.png'),
+(2, 'Anna Banana', '070-7770000', 'anna@theschool.com', 'owner', '456', 'upload/admin.png'),
+(3, 'Elsa Frozen', '090-1234567', 'elsa@brrr.com', 'admin', '123', 'upload/admin.png'),
+(4, 'aa', '070-7770000', 'aa', 'owner', 'aa', 'upload/admin.png');
 
 --
 -- Indexes for dumped tables
@@ -181,19 +182,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
